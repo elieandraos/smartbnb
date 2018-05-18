90c1e3568c74addb34bcb5efa9b5973a83d65590
Elie Andraos <elie@andraos.org>
2018-05-18 12:47:12 +0300

    created users migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,9 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->integer('bonus_percent')->default(0);
+            $table->integer('deposit_iteration')->default(0);
+            $table->boolean('is_admin')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
